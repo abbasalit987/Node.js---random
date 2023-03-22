07f38fc7b767000643b645063f81e57d650f9c62
abbasalit987 <abbasalit987@gmail.com>
2023-03-22 15:42:05 +0530

    commit 4/response routing

--- server.js
+++ server.js
@@ -3,6 +3,33 @@ const http = require('http')
 server = http.createServer((req, res) => {
     //console.log('Abbas');
     //res.write();
+
+    const url = req.url
+
+    if (url === '/home') {
+        res.write('<html>')
+        res.write('<head><title>Introduction Page</title></head>')
+        res.write('<body><h1>Welcome Home</h1></body>')
+        res.write('</html>')
+        return res.end()
+    }
+
+    else if (url === '/about') {
+        res.write('<html>')
+        res.write('<head><title>Introduction Page</title></head>')
+        res.write('<body><h1>Welcome to about us page</h1></body>')
+        res.write('</html>')
+        return res.end()
+    }
+
+    else if (url === '/node') {
+        res.write('<html>')
+        res.write('<head><title>Introduction Page</title></head>')
+        res.write('<body><h1>Welcome to my Node js Project</h1></body>')
+        res.write('</html>')
+        return res.end()
+    }
+
     res.setHeader('Content-Type','text/html')
     res.write('<html>')
     res.write('<head><title>Introduction Page</title></head>')
