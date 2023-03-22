4625fd383f1d90ebb55a37eb072e90a4d5f3c249
abbasalit987 <abbasalit987@gmail.com>
2023-03-22 15:27:04 +0530

    commit 3/response

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const http = require('http')
 
 server = http.createServer((req, res) => {
-    console.log('Abbas');
+    //console.log('Abbas');
+    //res.write();
+    res.setHeader('Content-Type','text/html')
+    res.write('<html>')
+    res.write('<head><title>Introduction Page</title></head>')
+    res.write('<body><h1>Hello from Node.js</h1></body>')
+    res.write('</html>')
+    res.end()
 })
 
 server.listen(4000)
