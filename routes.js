5132f38a64b288fc266fe99eb7cae919ff247873
abbasalit987 <abbasalit987@gmail.com>
2023-03-28 07:44:55 +0530

    commit 9/nodemon and debugger config changes

--- routes.js
+++ routes.js
@@ -38,9 +38,10 @@ const requestHandler = (req, res) => {
     req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       console.log(parsedBody)
-      const message = parsedBody.split('=')[1];
+      const message = parsedBody.split('=')[0];
       fs.writeFileSync('message.txt', message);
     });
+    
     res.statusCode = 302;
     res.setHeader('Location', '/');
     return res.end();
